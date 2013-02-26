b74271d04a79639af0dda57702342f87c8cc5c6c
Gabino3 <gdabdoub3@gmail.com>
2013-02-26 15:21:14 -0500

    Added 10x loop for thread creation

--- thread_gen.c
+++ thread_gen.c
@@ -1,42 +1,44 @@
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
+   #include <pthread.h>
+	#include <stdio.h>
+	#include <stdlib.h>
 
-/* this function is run by the second thread */
-void *foo(void *h)
-{
-   int i, n = 100;
-   int *list = malloc(n * sizeof(int));
+	/* this function is run by the second thread */
+	void *foo(void *h)
+	{
+	   int i, n = 100;
+	   int *list = malloc(n * sizeof(int));
 
-   for (i = 0; i < n; i++){
-      list[i] = i*i;
+	   for (i = 0; i < n; i++){
+		  list[i] = i*i;
 
-   }
-   printf("This is thread %d finishing 100 perfect squares\n", i);
+	   }
+	   printf("This is a thread finishing 100 perfect squares\n");
 
-   free(list);
+	   free(list);
 
-   pthread_exit(NULL);
-   return NULL;
+	   pthread_exit(NULL);
+	   return NULL;
 
-}
+	}
 
-int main()
-{
+	int main()
+	{
 
-   int i;
-   pthread_t *threads = malloc(sizeof(pthread_t)*10);
+	   int i, j;
+	   for(j=0;j<10;j++){
+		   pthread_t *threads = malloc(sizeof(pthread_t)*10);
 
 
-   for (i=0; i<10; i++){
-      if(pthread_create(&threads[i], NULL, foo, NULL)) {
-         fprintf(stderr, "Error creating thread\n");
-         return 1;
-      }
-   }
+		   for (i=0; i<10; i++){
+			  if(pthread_create(&threads[i], NULL, foo, NULL)) {
+				 fprintf(stderr, "Error creating thread\n");
+				 return 1;
+			  }
+		   }
 
-   for (i=0; i<10; i++){
-      pthread_join(threads[i], NULL);
-   }
-   free(threads);
-}
+		   for (i=0; i<10; i++){
+			  pthread_join(threads[i], NULL);
+		   }
+		   free(threads);
+	   }
+	}
